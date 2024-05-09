731acc14d2dd82ed19e71340de491424bd3c2b55
Arya M <armlk4711@gmail.com>
2024-05-08 21:54:15 -0400

    add mappings to move lines up and down

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,5 +14,6 @@ map("n", "<Down>", "<Nop>")
 map("n", "<Left>", "<Nop>")
 map("n", "<Right>", "<Nop>")
 map("n", "<Up>", "<Nop>")
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("n", "<A-k>", "<cmd>m.-2<CR>==")
+map("n", "<A-j>", "<cmd>m.+1<CR>==")
