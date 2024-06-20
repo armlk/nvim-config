7d860320c6081093f1580b1636545db17ab18a81
Arya M <armlk4711@gmail.com>
2024-06-20 14:28:31 -0400

    add C and Go support

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "tsserver", "tailwindcss", "eslint" }
+local servers = { "html", "cssls", "tsserver", "tailwindcss", "eslint", "gopls", "clangd" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
